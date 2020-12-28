ec195806119f2cf0557561610fd9ad0e871f4495
gozdetutann <71690165+gozdetutann@users.noreply.github.com>
2020-12-28 11:23:36 +0300

    Add files via upload

---
+++ harfNotu.py
@@ -0,0 +1,24 @@
+vize1 = input("1. Vize:")
+vize2 = input("2. Vize:")
+Final = input("Final:")
+
+sonuc = int(vize1)*(3/10) + int(vize2)*(3/10) + int(Final)*(4/10)
+
+if(sonuc >=90):
+    print("AA")
+elif(sonuc>=85):
+    print("BA")
+elif(sonuc>=80):
+    print("BB")
+elif(sonuc>=75):
+    print("CB")
+elif(sonuc>=70):
+    print("CC")
+elif(sonuc>=65):
+    print("DC")
+elif(sonuc>=60):
+    print("DD")
+elif(sonuc>=55):
+    print("FD")
+elif(sonuc>=50):
+    print("FF")
