d4686641f709d98cf782fc2c8b8396ea3b82dd2f
gozdetutann <71690165+gozdetutann@users.noreply.github.com>
2021-01-13 21:53:15 +0300

    Locators

---
+++ locators.py
@@ -0,0 +1,21 @@
+
+	#1.
+UTM_SETTINGS = (By.CSS_SELECTOR, '[for="enableUtmSettings"]')
+	#2.
+INPUT_WARNING_MESSAGE = (By.XPATH, '//*[contains(text(), "The Your Message field is required.")]')
+	#3.
+SEARCH_INPUT = (By.ID, 'search-value')
+	#4.
+TEST_RATIO_BUTTON = (By_ID, '#Test')
+	#5.
+CAMPAIGN_STATISTICS = (By.XPAH, '//*[@class = "d-f cur-p t-d-n"]')
+	#6.
+REMOVE_MESSAGE_BOX_VARIATION = (By.ID, '#delete')
+	#7.
+REMOVE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, '[class="optionImg"][5]')
+DELETE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, '[class="optionImg"][8]')
+	#8.
+ALERT_TOASTER = (By.CLASS_NAME, '[class="icon qa-icon fl-l mr-3 name in-toasts-wrapper_alert__icon"]')
+	#9.
+CREATE_SOCIAL_PROOF = (By_ID, '#create-mobile-campaign')
+
