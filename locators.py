6ed462e8c9fc7577f116a4b08e0558000a1fd4d7
gozdetutann <71690165+gozdetutann@users.noreply.github.com>
2021-01-14 10:41:49 +0300

    Locators - CR fix done

--- locators.py
+++ locators.py
@@ -6,16 +6,16 @@ INPUT_WARNING_MESSAGE = (By.XPATH, '//*[contains(text(), "The Your Message field
 	#3.
 SEARCH_INPUT = (By.ID, 'search-value')
 	#4.
-TEST_RATIO_BUTTON = (By_ID, '#Test')
+TEST_RATIO_BUTTON = (By.ID, 'Test')
 	#5.
-CAMPAIGN_STATISTICS = (By.XPAH, '//*[@class = "d-f cur-p t-d-n"]')
+CAMPAIGN_STATISTICS = (By.XPATH, '//*[@class = "d-f cur-p t-d-n"]')
 	#6.
-REMOVE_MESSAGE_BOX_VARIATION = (By.ID, '#delete')
+REMOVE_MESSAGE_BOX_VARIATION = (By.ID, 'delete')
 	#7.
-REMOVE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, '[class="optionImg"][5]')
-DELETE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, '[class="optionImg"][8]')
+REMOVE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, 'optionImg')
+DELETE_ELEMENT_ON_JOURNEY = (By.CLASS_NAME, 'optionImg')
 	#8.
-ALERT_TOASTER = (By.CLASS_NAME, '[class="icon qa-icon fl-l mr-3 name in-toasts-wrapper_alert__icon"]')
+ALERT_TOASTER = (By.CLASS_NAME, 'messageAlertBoxIcon')
 	#9.
-CREATE_SOCIAL_PROOF = (By_ID, '#create-mobile-campaign')
+CREATE_SOCIAL_PROOF = (By.ID, 'create-mobile-campaign')
 
